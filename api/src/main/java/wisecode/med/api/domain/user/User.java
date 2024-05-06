913ae868e3e3df79fe2771d33db032ab3d3f4f23
leonardo <leonardo.araujo@exago.com>
2024-05-06 15:36:13 -0300

    configuracao inicial spring security

---
+++ api/src/main/java/wisecode/med/api/domain/user/User.java
@@ -0,0 +1,24 @@
+package wisecode.med.api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+
+@Table(name = "user")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String login;
+    private String password;
+}
