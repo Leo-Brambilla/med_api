6056e1256f4d6e19d1bb98244c7703fca7cc0a1f
leonardo <leonardo.araujo@exago.com>
2024-05-08 12:40:46 -0300

    implementacao de mÃetodos para verificar token do usuario

--- api/src/main/java/wisecode/med/api/infra/security/TokenService.java
+++ api/src/main/java/wisecode/med/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package wisecode.med.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,20 @@ public class TokenService {
         } catch (JWTCreationException e) {
             throw new RuntimeException("erro ao gerar o token jwt", e);
         }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("WiseCode med.API")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+
     }
 
     private Instant expiresDate() {
